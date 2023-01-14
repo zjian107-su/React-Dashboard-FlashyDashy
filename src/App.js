dc079927da9df54188253956736f76c914275298
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-01-14 15:08:56 -0500

    ✨ update: project setup data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.css";
+
 const App = () => {
   return <h1 className="underline text-3xl">App</h1>;
 };
