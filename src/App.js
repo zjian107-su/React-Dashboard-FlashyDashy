c686e7c0646aa2cc81f30ec6b948bc2535d7e6e3
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-01-15 09:19:26 -0500

    ✨ update: setup fiseeting icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import "./App.css";
 
 const App = () => {
-  return <h1 className="underline text-3xl">App</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed bottom-4 right-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-grey text-white"
+                style={{ backgroundColor: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
