4beacd08ebbaad3b17ffd376d3c20f410f66d363
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-01-18 10:58:53 -0500

    ✨ update: page components init

---
+++ src/pages/Charts/ColorMatching.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ColorMatching = () => {
+  return <div>ColorMatching</div>;
+};
+
+export default ColorMatching;
---
+++ src/pages/Ecommer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ecommer = () => {
+  return <div>Ecommer</div>;
+};
+
+export default Ecommer;
