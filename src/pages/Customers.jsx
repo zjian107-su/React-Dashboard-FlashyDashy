66a208f25ffe089f688cf64168ce2cf2fb5dc194
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-01-26 09:11:47 -0500

    ✨ update: ecommerce banner and basic layout init

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Customers = () => {
-  return <div>Customer</div>;
+  return <div></div>;
 };
 
 export default Customers;
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,8 @@ module.exports = {
         590: "590px",
       },
       backgroundImage: {
-        "hero-pattern": "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')",
+        // "hero-pattern": "url('src/data/welcome-bg00.svg')",
+        "hero-pattern": "url('data/welcome-bg.svg')",
       },
     },
   },
