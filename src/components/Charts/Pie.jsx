e325b216abd90480ddb36f0e39583933cb621af6
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-02-11 09:34:55 -0500

    ✨ update: pie page

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,20 +1,62 @@
 import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  AccumulationLegend,
+  PieSeries,
+  AccumulationDataLabel,
+  Inject,
+  AccumulationTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-import { pieChartData } from "../../data/dummy";
-import { ChartsHeader, Pie as PieChart } from "../../components";
+import { useStateContext } from "../../contexts/ContextProvider";
 
-const Pie = () => (
-  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    <ChartsHeader category="Pie" title="Project Cost Breakdown" />
-    <div className="w-full">
-      <PieChart
-        id="chart-pie"
-        data={pieChartData}
-        legendVisiblity
-        height="full"
+const Doughnut = ({ id, data, legendVisiblity, height }) => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <AccumulationChartComponent
+      id={id}
+      legendSettings={{ visible: legendVisiblity, background: "white" }}
+      height={height}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      tooltip={{ enable: true }}
+    >
+      <Inject
+        services={[
+          AccumulationLegend,
+          PieSeries,
+          AccumulationDataLabel,
+          AccumulationTooltip,
+        ]}
       />
-    </div>
-  </div>
-);
+      <AccumulationSeriesCollectionDirective>
+        <AccumulationSeriesDirective
+          name="Sale"
+          dataSource={data}
+          xName="x"
+          yName="y"
+          innerRadius="40%"
+          startAngle={0}
+          endAngle={360}
+          radius="70%"
+          explode
+          explodeOffset="10%"
+          explodeIndex={2}
+          dataLabel={{
+            visible: true,
+            name: "text",
+            position: "Inside",
+            font: {
+              fontWeight: "600",
+              color: "#fff",
+            },
+          }}
+        />
+      </AccumulationSeriesCollectionDirective>
+    </AccumulationChartComponent>
+  );
+};
 
-export default Pie;
+export default Doughnut;
--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,62 +1,20 @@
 import React from "react";
-import {
-  AccumulationChartComponent,
-  AccumulationSeriesCollectionDirective,
-  AccumulationSeriesDirective,
-  AccumulationLegend,
-  PieSeries,
-  AccumulationDataLabel,
-  Inject,
-  AccumulationTooltip,
-} from "@syncfusion/ej2-react-charts";
 
-import { useStateContext } from "../../contexts/ContextProvider";
+import { pieChartData } from "../../data/dummy";
+import { ChartsHeader, Pie as PieChart } from "../../components";
 
-const Doughnut = ({ id, data, legendVisiblity, height }) => {
-  const { currentMode } = useStateContext();
-
-  return (
-    <AccumulationChartComponent
-      id={id}
-      legendSettings={{ visible: legendVisiblity, background: "white" }}
-      height={height}
-      background={currentMode === "Dark" ? "#33373E" : "#fff"}
-      tooltip={{ enable: true }}
-    >
-      <Inject
-        services={[
-          AccumulationLegend,
-          PieSeries,
-          AccumulationDataLabel,
-          AccumulationTooltip,
-        ]}
+const Pie = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Pie" title="Project Cost Breakdown" />
+    <div className="w-full">
+      <PieChart
+        id="chart-pie"
+        data={pieChartData}
+        legendVisiblity
+        height="full"
       />
-      <AccumulationSeriesCollectionDirective>
-        <AccumulationSeriesDirective
-          name="Sale"
-          dataSource={data}
-          xName="x"
-          yName="y"
-          innerRadius="40%"
-          startAngle={0}
-          endAngle={360}
-          radius="70%"
-          explode
-          explodeOffset="10%"
-          explodeIndex={2}
-          dataLabel={{
-            visible: true,
-            name: "text",
-            position: "Inside",
-            font: {
-              fontWeight: "600",
-              color: "#fff",
-            },
-          }}
-        />
-      </AccumulationSeriesCollectionDirective>
-    </AccumulationChartComponent>
-  );
-};
+    </div>
+  </div>
+);
 
-export default Doughnut;
+export default Pie;
